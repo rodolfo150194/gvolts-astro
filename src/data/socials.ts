66c73d1ec9ed4ef482d09592945fe0ba74945e89
rodolfo <rodolfogarciamaeso@gmail.com>
2025-12-01 18:16:34 -0500

    Agregando componentes [contacto + pagina, faqs, footer], corregido responsive landing

---
+++ src/data/socials.ts
@@ -0,0 +1,27 @@
+export interface SocialLink {
+  name: string;
+  icon: string;
+  url: string;
+  ariaLabel: string;
+}
+
+export const socials: SocialLink[] = [
+  {
+    name: "Facebook",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>`,
+    url: "https://facebook.com/gvolts",
+    ariaLabel: "Visita nuestra página de Facebook"
+  },
+  {
+    name: "Instagram",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>`,
+    url: "https://instagram.com/gvolts",
+    ariaLabel: "Síguenos en Instagram"
+  },
+  {
+    name: "Twitter",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4l11.733 16h4.267l-11.733 -16z"></path><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path></svg>`,
+    url: "https://twitter.com/gvolts",
+    ariaLabel: "Síguenos en Twitter"
+  }
+];
